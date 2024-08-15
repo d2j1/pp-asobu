c81196178d6a737c606734d2a7cbbccf038253db
Dhananjay <dhananjayjadhav2151@gmail.com>
2024-08-15 15:30:43 +0530

    basic request handling added

--- src/main/java/com/httpserver/HttpServer.java
+++ src/main/java/com/httpserver/HttpServer.java
@@ -3,6 +3,12 @@ package com.httpserver;
 import com.httpserver.config.Configuration;
 import com.httpserver.config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class HttpServer {
 
     public static void main(String[] args) {
@@ -10,8 +16,38 @@ public class HttpServer {
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
 
-//        System.out.println(conf.getPort());
-//        System.out.println(conf.getWebroute());
+        System.out.println(conf.getPort());
+        System.out.println(conf.getWebroute());
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept();
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            // read the request
+
+            // write the response
+            String html = "<html> <head><title> Abuso Server</title> </head> <body>This is the response returned by the abuso server </body></html>";
+
+            final String CRLF = "\n\r";
+
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF + // status line - it has http version response_code response_message
+                    "Content-Length: "+ html.getBytes().length + CRLF // header
+                    + CRLF +
+                            html +
+                            CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();;
+            outputStream.close();
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
