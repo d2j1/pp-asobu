9e3e8d9abfe0dcb9473fb507814172121c23da86
Dhananjay <dhananjayjadhav2151@gmail.com>
2024-08-27 21:36:35 +0530

    XMLParser added

---
+++ src/main/java/com/httpserver/util/XMLParser.java
@@ -0,0 +1,15 @@
+package com.httpserver.util;
+
+/**
+ * Class name - XMLParser
+ *
+ * <p>This is XMLParser class.  </p>
+ *
+ * @author Dhananjay
+ * @date 8/27/2024
+ * @updated 8/27/2024
+ */
+
+public class XMLParser {
+    
+}
